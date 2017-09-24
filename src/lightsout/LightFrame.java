da3b49e7bd071c67457ca440bcb9bf01a097e414
C. Arpe <colin.j.arpe@outlook.com>
2017-09-24 00:08:57 -0400

    Randomise opening game board

--- src/lightsout/LightFrame.java
+++ src/lightsout/LightFrame.java
@@ -8,6 +8,11 @@ package lightsout;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.IOException;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -15,23 +20,35 @@ import java.awt.event.*;
  */
 public class LightFrame extends JFrame  {
     LightGame thisGame = new LightGame (this);
-    JButton[] buttons = new JButton[60];
+    final int WIDTH = 10;
+    final int HEIGHT = 6;
+    JButton[][] buttons = new JButton[WIDTH][HEIGHT];
+    final Random rand = new Random();
+    public String btnText = "";
+    
+    Icon imgOn  = new ImageIcon(getClass().getResource("/images/button-on.jpg"));
+    Icon imgOff = new ImageIcon(getClass().getResource("/images/button-off.jpg"));
     
     public LightFrame() {
         super("lightsOut!");
-        setSize(400, 240);
+        setSize(500,300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         JPanel gameWindow = new JPanel();
-        GridLayout buttonLayout = new GridLayout(6, 10);
+        GridLayout buttonLayout = new GridLayout(HEIGHT, WIDTH);
         gameWindow.setLayout(buttonLayout);
         add(gameWindow);
         
-        for (int i = 0; i < 60; i++)    {
-            JButton nextBtn = new JButton("");
-//            nextBtn.addActionListener(thisGame);
-            gameWindow.add(nextBtn);
-            buttons[i] = nextBtn;
+        for (int i = 0; i < WIDTH; i++)    {
+            for (int j = 0; j < HEIGHT; j++)    {
+                btnText = rand.nextBoolean() ? "X" : "O";
+                JButton nextBtn = new JButton(btnText);
+//                nextBtn.setContentAreaFilled(false);
+//                nextBtn.setIcon(imgOn);
+    //            nextBtn.addActionListener(thisGame);
+                gameWindow.add(nextBtn);
+                buttons[i][j] = nextBtn;
+            }
         }
         
         setVisible(true);
